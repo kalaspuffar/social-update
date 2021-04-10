d89a6469a8c3045b3517f2dea3dae7e038b89d9c
Daniel Persson <mailto.woden@gmail.com>
2021-04-10 15:18:58 +0200

    Update wordpress.

--- wordpress_update.php
+++ wordpress_update.php
@@ -29,10 +29,33 @@ $my_post = array(
     'post_content'  => $content,
     'post_status'   => 'publish',
     'post_author'   => 1,
-    'post_category' => array()
+    'post_category' => array(2)
 );
 
 // Insert the post into the database
-wp_insert_post( $my_post );
+$post_id = wp_insert_post( $my_post );
+
+$filename = basename($youtube_data->youtube_url);
+$upload_file = wp_upload_bits($filename, null, file_get_contents($youtube_data->youtube_url));
+if (!$upload_file['error']) {
+    $wp_filetype = wp_check_filetype($filename, null );
+    $attachment = array(
+        'post_mime_type' => $wp_filetype['type'],
+        'post_parent' => 0,
+        'post_title' => preg_replace('/\.[^.]+$/', '', $filename),
+        'post_content' => '',
+        'post_status' => 'inherit'
+    );
+
+    $attachment_id = wp_insert_attachment( $attachment, $upload_file['file'], $post_id );
+
+    if (!is_wp_error($attachment_id)) {
+        require_once(ABSPATH . 'wp-admin/includes/image.php');
+        $attachment_data = wp_generate_attachment_metadata( $attachment_id, $upload_file['file'] );
+        wp_update_attachment_metadata( $attachment_id,  $attachment_data );
+    }
+
+    set_post_thumbnail( $post_id, $attachment_id );
+}
 
 file_put_contents($WORDPRESS_STATUS_FILE, $youtube_id);
